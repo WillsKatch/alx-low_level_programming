5fe76a50dbc5a9c8dfc2e862f25a135678a8ec42
WillsKacth <wills.tchapga@gmail.com>
2022-07-03 19:10:46 -0700

    hope

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -35,11 +35,13 @@ int main(void)
 					putchar('0' + e);
 					putchar('0' + d);
 					putchar('0' + c);
+
 					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
 					}
+
 				}
 				c++;
 			}
