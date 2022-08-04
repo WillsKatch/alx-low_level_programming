bbbfd522d7db626309ef8a1f1bb69c21123518ae
WillsKacth <wills.tchapga@gmail.com>
2022-08-04 05:33:36 -0700

    katch

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- * op_add - Add two integers
+ * op_add - Struct op
  *
- * @a: Integer 1
- * @b: Integer 2
- *
- * Return: Integer
+ * @a: The operator
+ * @b: The function associated
+ * Return: int
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - Sub two integers
+ * op_sub - Struct op
  *
- * @a: Integer 1
- * @b: Integer 2
- *
- * Return: Integer
+ * @a: The operator
+ * @b: The function associated
+ * Return: int
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - Product of two integers
+ * op_mul - Struct op
  *
- * @a: Integer 1
- * @b: Integer 2
- *
- * Return: Integer
+ * @a: The operator
+ * @b: The function associated
+ * Return: int
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_div - Result of the dividion os two integers
+ * op_div - Struct op
  *
- * @a: Integer 1
- * @b: Integer 2
- *
- * Return: Integer
+ * @a: The operator
+ * @b: The function associated
+ * Return: int
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 /**
- * op_mod - Remainder of the division
+ * op_mod - Struct op
  *
- * @a: Integer 1
- * @b: Integer 2
- *
- * Return: Integer
+ * @a: The operator
+ * @b: The function associated
+ * Return: int
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
